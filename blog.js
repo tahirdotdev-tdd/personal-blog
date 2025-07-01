40171c54cd2a3896e7bfd6f9784a81b610516c74
Tahir Hassan <chatwithme2344@gmail.com>
2025-07-01 09:26:00 +0500

    enw

--- blog.js
+++ blog.js
@@ -6,20 +6,38 @@ const posts = [
 const postList = document.getElementById('post-list');
 const postContent = document.getElementById('post-content');
 
-posts.forEach(post => {
-  const link = document.createElement('a');
-  link.href = '#';
-  link.innerText = post.replace('.md', '').replace(/-/g, ' ');
-  link.onclick = () => loadPost(post);
-  const div = document.createElement('div');
-  div.appendChild(link);
-  postList.appendChild(div);
-});
+function renderPostList() {
+  postList.innerHTML = '';
+  posts.forEach(post => {
+    const link = document.createElement('a');
+    link.href = '#';
+    link.innerText = post.replace('.md', '').replace(/-/g, ' ');
+    link.onclick = (e) => {
+      e.preventDefault();
+      loadPost(post);
+    };
+    const div = document.createElement('div');
+    div.appendChild(link);
+    postList.appendChild(div);
+  });
+}
 
 function loadPost(filename) {
   fetch(`posts/${filename}`)
     .then(res => res.text())
     .then(data => {
+      postList.style.display = 'none'; // hide list
       postContent.innerText = data;
+
+      // Add a back button
+      const backBtn = document.createElement('button');
+      backBtn.innerText = '← Back to all posts';
+      backBtn.onclick = () => {
+        postContent.innerHTML = '';
+        postList.style.display = 'block';
+      };
+      postContent.prepend(backBtn);
     });
 }
+
+renderPostList();
